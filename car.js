b0f547bfe07d4386887bbcb56560e13764753a38
VARSHA-pn <83294552+VARSHA-pn@users.noreply.github.com>
2023-05-13 21:20:19 +0200

    Add files via upload

---
+++ car.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent("car-model", 
+{
+    schema: 
+    {
+        modelRef: {type: "string", default: "../car_model/scene.gltf"}
+    },
+    init: function()
+    {
+        this.el.setAttribute("gltf-model", this.data.modelRef);
+        const position = {x: 0, y: 1, z: 0};
+        const rotation = {x: 0, y: -100, z: 0};
+        this.el.setAttribute("position", position);
+        this.el.setAttribute("rotation", rotation);
+    }
+});
